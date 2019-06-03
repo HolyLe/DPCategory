b717c2413fbec8a2f8410da5b79152a020a3b4c0
麻小亮 <maxiaoliang@able-elec.com>
2019-06-03 19:52:49 +0800

    no message

--- DPCategory/DPCategory/ViewController.m
+++ DPCategory/DPCategory/ViewController.m
@@ -7,15 +7,29 @@
 //
 
 #import "ViewController.h"
-
+#import "DPCategory.h"
 @interface ViewController ()
-
+@property (nonatomic, strong) UILabel * label;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabelCreate()
+    .makeChain
+    .addToSuperView(self.view)
+    .backgroundColor([UIColor grayColor])
+    //如果项目导入masonry的话
+//    .makeMasonry(^(MASConstraintMaker * _Nonnull make) {
+//        make.top.mas_offset(16);
+//        make.centerX.equalTo(self.contentView);
+//        make.width.height.mas_equalTo(100);
+//    })
+    .assignTo(^(__kindof UIView * _Nonnull view) {
+        self.label = view;
+    });
+;
     // Do any additional setup after loading the view.
 }
 
